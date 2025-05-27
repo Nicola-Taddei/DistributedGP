b2bd6b98f7354ad55a2f37be8a7c28a939cd9536
Nicola-Taddei <nicola.taddei01@gmail.com>
2025-05-27 14:40:16 +0200

    Cleaner code for plots

---
+++ utils/utils/utils.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
+from functools import partial
+
+def plot_field(
+        ax,
+        x,
+        y,
+        z,
+        title,
+        cmap,
+        norm,
+        levels,
+        pos_ticks,
+        ticks,
+        extend,
+        drone_pos,
+        drone_color,
+        font_size):
+    im = ax.contourf(x, y, z, cmap=cmap, norm=norm, levels=levels)
+    ax.scatter(*drone_pos, c=drone_color)
+    sm = cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])
+    cbar = plt.colorbar(sm, extend=extend, ax=ax)
+    cbar.set_ticks(ticks)
+    cbar.ax.tick_params(labelsize=font_size)
+    ax.set_title(title, fontsize=font_size)
+    ax.set_xlabel("x", fontsize=font_size)
+    ax.set_ylabel("y", fontsize=font_size)
+    ax.set_xticks(pos_ticks)
+    ax.set_yticks(pos_ticks)
+    ax.tick_params(labelsize=font_size)
+
+# Let's define some constants for plot rendering
+
+# Normalize so that:
+# -1 → blue (low),  0 → white (mid),  1 → red (high)
+norm = mcolors.TwoSlopeNorm(vmin=-1, vcenter=0, vmax=1)
+levels=100
+cmap = 'coolwarm'
+var_cmap = 'OrRd'
+drone_color = 'yellow'
+font_size = 14
+pos_ticks = [0, 100]
+pred_ticks = [-1, 0, +1]
+var_ticks = [0, +1]
+
+plot_mean = partial(
+    plot_field,
+    cmap=cmap,
+    norm=norm,
+    levels=levels,
+    pos_ticks=pos_ticks,
+    ticks=pred_ticks,
+    extend='both',
+    drone_color=drone_color,
+    font_size=font_size
+)
+
+plot_var = partial(
+    plot_field,
+    cmap=var_cmap,
+    norm=norm,
+    levels=levels,
+    pos_ticks=pos_ticks,
+    ticks=var_ticks,
+    extend='max',
+    drone_color=drone_color,
+    font_size=font_size
+)
