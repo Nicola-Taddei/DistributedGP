68573cf4e70ee8b0b56e4bb3a447b39f0651e06d
Nicola-Taddei <nicola.taddei01@gmail.com>
2025-05-28 22:06:24 +0200

    Improved figures

--- utils/utils/utils.py
+++ utils/utils/utils.py
@@ -13,28 +13,40 @@ def plot_field(
         cmap,
         norm,
         levels,
-        pos_ticks,
-        ticks,
-        extend,
-        drone_color,
         font_size,
+        pos_ticks=None,
+        ticks=None,
+        extend=None,
+        drone_color=None,
         title=None,
-        drone_pos=None):
+        drone_pos=None,
+        plot_cbar=True,
+        plot_axes_labels=True,
+        plot_x_ticks=True,
+        plot_y_ticks=True):
     im = ax.contourf(x, y, z, cmap=cmap, norm=norm, levels=levels)
     if drone_pos is not None:
         ax.scatter(*drone_pos, c=drone_color)
     sm = cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
-    cbar = plt.colorbar(sm, extend=extend, ax=ax)
-    cbar.formatter = ticker.FormatStrFormatter('%.2f')
-    cbar.set_ticks(ticks)
-    cbar.ax.tick_params(labelsize=font_size)
+    if plot_cbar:
+        cbar = plt.colorbar(sm, extend=extend, ax=ax)
+        cbar.formatter = ticker.FormatStrFormatter('%.2f')
+        cbar.set_ticks(ticks)
+        cbar.ax.tick_params(labelsize=font_size)
     if title is not None:
         ax.set_title(title, fontsize=font_size)
-    ax.set_xlabel("x", fontsize=font_size)
-    ax.set_ylabel("y", fontsize=font_size)
-    ax.set_xticks(pos_ticks)
-    ax.set_yticks(pos_ticks)
+    if plot_axes_labels:
+        ax.set_xlabel("x", fontsize=font_size)
+        ax.set_ylabel("y", fontsize=font_size)
+    if plot_x_ticks:
+        ax.set_xticks(pos_ticks)
+    else:
+        ax.set_xticks([])
+    if plot_y_ticks:
+        ax.set_yticks(pos_ticks)
+    else:
+        ax.set_yticks([])
     ax.tick_params(labelsize=font_size)
 
 # Let's define some constants for plot rendering
@@ -43,7 +55,7 @@ def plot_field(
 # -1 → blue (low),  0 → white (mid),  1 → red (high)
 norm = mcolors.TwoSlopeNorm(vmin=-1, vcenter=0, vmax=1)
 var_norm = mcolors.TwoSlopeNorm(vmin=0, vcenter=0.5, vmax=1)
-levels=100
+levels=300
 cmap = 'coolwarm'
 var_cmap = 'OrRd'
 drone_color = 'yellow'
@@ -58,7 +70,11 @@ def plot_var_free(
         y,
         z,
         title=None,
-        drone_pos=None):
+        drone_pos=None,
+        plot_cbar=True,
+        plot_axes_labels=True,
+        plot_x_ticks=True,
+        plot_y_ticks=True):
     limit = np.max(z)
     plot_field(
         ax,
@@ -74,7 +90,11 @@ def plot_var_free(
         extend='max',
         drone_color=drone_color,
         font_size=font_size,
-        drone_pos=drone_pos
+        drone_pos=drone_pos,
+        plot_cbar=plot_cbar,
+        plot_axes_labels=plot_axes_labels,
+        plot_x_ticks=plot_x_ticks,
+        plot_y_ticks=plot_y_ticks
     )
 
 plot_mean = partial(
